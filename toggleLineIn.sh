01ef843cb287635f67510260d2b7dd5d20816457
Jon Howe <jonhowe@users.noreply.github.com>
2018-12-11 13:04:24 -0500

    Update toggleLineIn.sh

--- toggleLineIn.sh
+++ toggleLineIn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #http://www.virtjunkie.com/ubuntu---mute-line-in-using-keyboard/
-
+#https://github.com/jonhowe/Virtjunkie.com/blob/master/toggleLineIn.sh
 
 OUT=$(amixer sset Line toggle)
 RESULT=$(echo $OUT | grep "Left:" | cut -d'[' -f4 | cut -d ']' -f 1)
